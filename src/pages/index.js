1efb168a17c5ee76841401e7e70a9b02f29f8d10
nick-feltham <nick@rkkcreative.xyz>
2019-05-03 22:43:37 +0300

    tetsing commit to the master
    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,10 @@ import React from "react"
 import Form from "../components/Form"
 
 const IndexPage = () => (
+  <div>
     <Form />
+    <p>This is a test to the master branch</p>
+  </div>
 )
 
 export default IndexPage
